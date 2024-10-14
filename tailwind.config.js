6444812d482ff32bdf3161d6d3fb551f5a96f95f
pascal <pascalokereke18@gmail.com>
2024-10-14 23:47:51 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     screens: {
       'xs': '375px',
-      'sm': '640px'.
-      'md': '768px'.
+      'sm': '640px',
+      'md': '768px',
       'lg': '1024px',
       'xl': '1289px',
     },
